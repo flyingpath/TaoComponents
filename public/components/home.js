0a04ca502aeca0def69cb3da1fd52b332248bf37
tao <flyingpath@gmail.com>
2017-12-27 15:05:19 +0800

    update

--- public/components/home.js
+++ public/components/home.js
@@ -3,7 +3,7 @@ import { observer } from 'mobx-react'
 import mobx from 'mobx'
 import _ from 'lodash'
 
-import T_RoundButton from 'tao-components'
+import {T_RoundButton} from 'tao-components'
 import dataStore from '../stores/data'
 
 class Home extends React.Component {
