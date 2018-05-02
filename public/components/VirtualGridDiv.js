fc680b6cb5c1abf1c81391d1db8be0ee74bed84d
tao <flyingpath@gmail.com>
2018-05-02 11:00:06 +0800

    update

--- public/components/VirtualGridDiv.js
+++ public/components/VirtualGridDiv.js
@@ -1,5 +1,6 @@
 import React from 'react' 
 import _ from 'lodash' 
+import moment from 'moment'
 
 import { AutoSizer, Table, Column } from 'react-virtualized'
 import 'react-virtualized/styles.css'
@@ -10,17 +11,129 @@ class VirtualTable extends React.Component {
     constructor(props) {
         super(props)
         
-        this.rawDataItem = props.dataItem
-        this.columnData = _.isEmpty( this.rawDataItem )? [] : this.rawDataItem.tableColumns
-        this.rawData = _.isEmpty( this.rawDataItem )? [] : this.rawDataItem.data
-        this.rawDecodeData = this.decodeData(this.columnData, this.rawData)
+        this.columnData    = []
+        this.rawData       = []
+
+        this.rawDecodeData = []
+        this.mode = 'big'
         
         this.state = {
-            data: this.rawDecodeData,
-            sortBy: '',
+            rawDataItem  : {},
+            columnData   : [],
+            rawData      : [],
+            rawDecodeData: [],
+            data         : [],
+            sortBy       : '',
+            sortDirection: 'ASC',
+            height       : window.innerHeight<700? 500:props.height
+        }
+    }
+
+    static decodeData(columnData, data){
+        
+        return data.map( (eachData)=>{
+
+            let searchStr = ''
+
+            columnData.map( (eachColumn)=>{
+
+                let value = eachData[ eachColumn.dataKey ]
+                
+                if ( eachColumn.falseDecode && !value ){
+                    value = eachColumn.falseDecode
+                } else if ( eachColumn.trueDecode && value ){
+                    value = eachColumn.trueDecode
+                }
+                
+                if ( eachColumn.decodes[value] ){
+                    value = eachColumn.decodes[value]
+                }
+                
+                eachData[ eachColumn.dataKey ] = value
+
+                searchStr += String(value)
+            } )
+
+            eachData.searchStr = searchStr
+
+            return eachData
+        } )
+    }
+
+    static getDerivedStateFromProps(nextProps, prevState){
+
+        const rawDataItem = nextProps.dataItem || {}
+        let columnData  = _.isEmpty( rawDataItem )? [] : rawDataItem.tableColumns
+        const rawData     = _.isEmpty( rawDataItem )? [] : rawDataItem.data
+
+        if (window.innerWidth < 700){
+        
+        //---- 響應示不出現的 column ----//
+            const notMatterList = [
+                '員編', '部門', '職稱', '上班卡'
+            ]
+            columnData = columnData.filter( (x)=>{
+                return !(notMatterList.indexOf(x.label) > -1)
+            } )
+        }
+        //----------------------------//
+
+        const rawDecodeData = T_Table.decodeData(columnData, rawData)
+        
+        return {
+            rawDataItem  : rawDataItem,
+            columnData   : columnData,
+            rawData      : rawData,
+            rawDecodeData: rawDecodeData,
+            data         : rawDecodeData,
+            sortBy       : '',
             sortDirection: 'ASC'
         }
-        // this._rowClassName = this._rowClassName.bind(this)
+    }
+
+    componentDidMount(){
+        this.reSizeData()
+        window.onresize = () => {
+            if(window.innerWidth<700 && this.mode=='big'){
+                this.reSizeData()
+                this.mode = 'small'
+            }else if (window.innerWidth>700 && this.mode=='small'){
+                this.reSizeData()
+                this.mode = 'big'
+            }
+        } 
+    }
+
+    reSizeData(){
+        if (window.innerWidth<700){
+        //---- 響應示不出現的 column ----//
+            const notMatterList = [
+                '員編', '部門', '職稱', '上班卡'
+            ]
+        //----------------------------//
+            
+            const newTableColumn = this.state.columnData.filter( (x)=>{
+                return !(notMatterList.indexOf(x.label) > -1)
+            } )
+            
+            this.setState({
+                columnData: newTableColumn
+            })
+        }else{
+            this.setState({
+                columnData: _.isEmpty( this.state.rawDataItem )? [] : this.state.rawDataItem.tableColumns
+            })
+        }
+
+        if(window.innerHeight<700){
+            this.setState({
+                height: 500
+            })
+        }else{
+            this.setState({
+                height: this.props.height
+            })
+        }
     }
 
     rowGetter = (props) => {
@@ -29,13 +142,13 @@ class VirtualTable extends React.Component {
     }
 
     getRowHeight  = () =>{
-        return 50
+        return 80
     }
 
     search =(e)=>{
         const searchStr = e.target.value
         
-        let data = this.rawDecodeData
+        let data = this.state.rawDecodeData
 
         if ( !searchStr ){
             this.setState({
@@ -72,44 +185,10 @@ class VirtualTable extends React.Component {
 
     noRowsRenderer = () =>{
         return (
-            <div>
-                no row 
+            <div style={{width:'100%', textAlign:'center', color: 'gray'}}>
+                {this.props.noRowsRenderer }
             </div>
         )
-    }
-
-    decodeData = (columnData, data) =>{
-        
-        return data.map( (eachData)=>{
-
-            let searchStr = ''
-
-            columnData.map( (eachColumn)=>{
-                
-                let value = eachData[ eachColumn.dataKey ]
-
-                if ( eachColumn.falseDecode && !value ){
-                    value = eachColumn.falseDecode
-                }
-        
-                if ( eachColumn.trueDecode && value ){
-                    value = eachColumn.trueDecode
-                }
-        
-                if ( eachColumn.decodes[value] ){
-                    value = eachColumn.decodes[value]
-                }
-
-                eachData[ eachColumn.dataKey ] = value
-
-                searchStr += String(value)
-            } )
-
-            eachData.searchStr = searchStr
-
-            return eachData
-        } )
-
     }
 
     rowRenderer = (props) => {
@@ -118,10 +197,15 @@ class VirtualTable extends React.Component {
 
         const propsColumnData = props.columns
 
+        let rowClassName = props.className + ' ' + this.props.rowClassName
+        if ( !props.rowData.isOnline ){
+            rowClassName += ' isOffLine'
+        }
+
         return (
             <div 
                 style={ props.style }
-                className = {props.className + ' ' + this.props.rowClassName}
+                className = {rowClassName}
                 key={props.key}
                 aria-label = 'row'
                 role="row"
@@ -131,7 +215,14 @@ class VirtualTable extends React.Component {
                     columnData.map( ( eachColumn, idx )=>{
 
                         let columnProps = _.cloneDeep(propsColumnData[idx].props)
+                        
                         columnProps.className = columnProps.className + ' ' + this.props.rowColumnClassName
+
+                        if (eachColumn.dataKey ==='isRest' && data.isRest){
+                            const start = moment(data.rest.start, 'YYYYMMDDHHmm')
+                            const end   = moment(data.rest.end, 'YYYYMMDDHHmm')
+                            columnProps.title = start.format('MM/DD HH:mm') + '\n' + end.format('MM/DD HH:mm')
+                        }
 
                         return(
                             <div {...columnProps} key={idx}>
@@ -147,48 +238,42 @@ class VirtualTable extends React.Component {
 
     render() {
 
-        if (!this.rawDataItem){
+        if (!this.state.rawDataItem){
             return(
-                <div>
+                <div >
                     沒有資料物件
                 </div>
             )
         }
         
         const data = this.state.data
-        const columnData = this.columnData
+        const columnData = this.state.columnData
 
         const rowCount = data.length
         
-        const height              = this.props.height
         const headerHeight        = this.props.headerHeight
         const overscanRowCount    = this.props.overscanRowCount
         const useDynamicRowHeight = true
-        const disableHeader = false
         const hideIndexRow  = false
-      
-        console.log(this.state.sortDirection)
-
         return (
             <div className={this.props.outterDivClassName}>
             <div className={this.props.searchDivClassName}>
                 <input type="text" onChange={this.search} className={this.props.searchInputClassName} />
             </div>
             <AutoSizer disableHeight>
-                {({width}) => (
+                {({width} ) => (
                 <Table
+                    ref="Table"
                     className = { this.props.tableClassName }
                     width={width}
-                    height={height}
-                    disableHeader={disableHeader}
+                    height={this.state.height}
                     headerClassName={ this.props.headerClassName }
                     headerHeight={headerHeight}
                     
+                    rowRenderer={ this.rowRenderer }
                     noRowsRenderer={this.noRowsRenderer}
                     overscanRowCount={overscanRowCount}
-                    rowClassName={this._rowClassName}
                     rowHeight={ this.getRowHeight }
-                    rowRenderer={ this.rowRenderer }
                     rowGetter = { this.rowGetter }
                     rowCount={rowCount}
                     
@@ -218,85 +303,23 @@ class VirtualTable extends React.Component {
     }
 }
 
-VirtualTable.defaultProps={
+T_Table.defaultProps={
 
-    height: 600,
-    headerHeight: 25,
+    height: 800,
+    headerHeight: 80,
     overscanRowCount: 4,
     
-    outterDivClassName: 'virtualGridTableDiv',
-    tableClassName: "virtualGridTable",
-    headerClassName: "virtualGridTableHeader",
-    tableClassName: "virtualGridTable",
-    rowClassName: "T_VirtualGridDiv_Row",
-    rowColumnClassName: "T_VirtualGridDiv_RowColumn",
-    searchDivClassName: "searchDivClassName",
+    outterDivClassName  : 'virtualGridTableDiv',
+    tableClassName      : "virtualGridTable",
+    headerClassName     : "virtualGridTableHeader",
+    tableClassName      : "virtualGridTable",
+    rowClassName        : "T_VirtualGridDiv_Row",
+    rowColumnClassName  : "T_VirtualGridDiv_RowColumn",
+    searchDivClassName  : "searchDivClassName",
     searchInputClassName: "searchInputClassName",
-    
+    noRowsRenderer      : "沒有所屬員工",
     dataItem: {
-        tableName: '出勤通知',
-
-        tableColumns: [
-            {
-                label: '員編',
-                dataKey: 'empno',
-                trueDecode: null,
-                falseDecode: null,
-                width: 70,
-                decodes: {
-                }
-            },
-            {
-                label: '姓名',
-                dataKey: 'name',
-                trueDecode: null,
-                falseDecode: null,
-                width: 70,
-                decodes: {
-
-                }
-            },
-            {
-                label: '職稱',
-                dataKey: 'title',
-                trueDecode: null,
-                falseDecode: null,
-                width: 70,
-                decodes: {
-                    '主任': '是主任唷'
-                }
-            },
-            {
-                label: '是否有上班',
-                dataKey: 'isWorkTime',
-                trueDecode: '有',
-                falseDecode: '無',
-                width: 80,
-                decodes: {
-                }
-            }
-        ],
-
-
-        data:[
-            {
-                "name": "戴文彬",
-                "empno": "001961",
-                "dept": "資訊部",
-                "title": "主任",
-                "isWorkTime": true,
-            },
-            {
-                "name": "林俊偉",
-                "empno": "002057",
-                "dept": "資訊部",
-                "title": "主任",
-                "isWorkTime": true,
-            }
-        ],
-
-        
     }
 }
 
-export default VirtualTable
+export default T_Table
